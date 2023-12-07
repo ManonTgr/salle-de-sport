89d87342ae45fe4e462d1536e2efef62e591101d
manonphi@hotmail.fr <manonphi@hotmail.fr>
2023-12-07 15:50:13 +0100

    mise en place des images

--- resources/views/acceuil.blade.php
+++ resources/views/acceuil.blade.php
@@ -6,6 +6,7 @@
 
 <h2> Page d'acceuil </h2>
 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium reprehenderit explicabo magni iusto excepturi dicta, illo cumque neque amet soluta tempora voluptatibus ipsum modi corrupti facilis enim corporis porro incidunt?</p>
-<a href="/img"
+<img src="img/evan.jpg" alt="Image salle"> 
+<img src="img/sven.jpg" alt="Image salle"> 
 
 @endsection
