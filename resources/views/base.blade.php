e50eb195d48dc2f6d004521348788ecf11e73844
manonphi@hotmail.fr <manonphi@hotmail.fr>
2023-12-07 12:44:39 +0100

    Ajout des liens vers les pages

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,6 +8,15 @@
     @vite(['resources/css/site-salle-sport.css', 'resources/js/site-salle-sport.js'])
 </head>
 <body>
-    
+    <header>
+        <h1>Site Salle de Sport</h1>
+        <nav>
+            <a href="{{ route('acceuil') }}">Aller vers la page Acceuil</a>
+            <a href="{{ route('cours') }}">Aller vers la page cours</a>
+            <a href="{{ route('tarif') }}">Aller vers la page tarif</a>
+            <a href="{{ route('contact') }}">Aller vers la page contact</a>
+            <a href="{{ route('mentions-legales') }}">Aller vers la page Mentions Légales</a>
+        </nav>
+    </header>
 </body>
 </html>
